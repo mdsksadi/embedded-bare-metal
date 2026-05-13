64c65f67e669058d12754f1581cbcfd449899fa7
sadi <sadiewu12345@gmail.com>
2026-05-13 19:09:29 +0200

    gpio pin setup added in firmware.c file

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -3,6 +3,7 @@ Blink the onboard LED
 */
 
 #include <libopencm3/stm32/rcc.h>   // Include the header for Reset and Clock Control
+#include <libopencm3/stm32/gpio.h>  // Include the header for General Purpose I/O
 
 /*
  * Setup the Reset and Clock Control
@@ -11,6 +12,11 @@ Blink the onboard LED
 static void rcc_setup(void)
 {
     rcc_clock_setup_pll(&rcc_hsi16_configs[RCC_CLOCK_3V3_80MHZ]);   // Set up the clock using the HSI16 oscillator and PLL to achieve 80 MHz system clock
+}
+
+static void gpio_setup(void)
+{
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);  // Configure GPIO pin B0 as an output with no pull-up or pull-down resistors
 }
 
 int main(void)
